41f5be05246c4ea4a519ed04f5fd963fca53da5b
udarasan <udarassanjeewa@gmail.com>
2023-11-29 16:51:35 +0530

    Initial commit

---
+++ src/main/java/com/example/brainanalizer/ElectrodeQualityController.java
@@ -0,0 +1,5 @@
+package com.example.brainanalizer;/**
+ * @TimeStamp 2023-11-29 16:41
+ * @ProjectDetails brain-analizer
+ */public class ElectrodeQualityController {
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.brainanalizer {
+    requires javafx.controls;
+    requires javafx.fxml;
+            
+                            
+    opens com.example.brainanalizer to javafx.fxml;
+    exports com.example.brainanalizer;
+}
