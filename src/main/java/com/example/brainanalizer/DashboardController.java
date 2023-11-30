7e622b1e65fe4bfab302206ff58e92c0f37f1c8c
udarasan <udarassanjeewa@gmail.com>
2023-11-30 18:04:29 +0530

    connection

--- src/main/java/com/example/brainanalizer/DashboardController.java
+++ src/main/java/com/example/brainanalizer/DashboardController.java
@@ -6,13 +6,16 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ListView;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
-import jssc.SerialPortList;
+import jssc.*;
 
 import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.ResourceBundle;
 
 /**
@@ -20,9 +23,13 @@ import java.util.ResourceBundle;
  * @ProjectDetails brain-analizer
  */
 public class DashboardController implements Initializable {
+
     public Pane context;
     public ListView panelId;
-
+    public TextField portID;
+    public TextArea messageArea;
+    private SerialPort serialPort;
+    ObservableList<String> devices = FXCollections.observableArrayList();
     private void setUi(String location) throws IOException {
         context.getChildren().clear();
         context.getChildren().add(FXMLLoader.
@@ -44,9 +51,53 @@ public class DashboardController implements Initializable {
 
 
     public void searchPort(ActionEvent actionEvent) {
+      connect(portID.getText());
+    }
 
+    public void connect(String portName) {
+        serialPort = new SerialPort(portName);
+        try {
+            serialPort.openPort();
+            serialPort.setParams(
+                    SerialPort.BAUDRATE_9600,
+                    SerialPort.DATABITS_8,
+                    SerialPort.STOPBITS_1,
+                    SerialPort.PARITY_NONE
+            );
+            System.out.println("Done");
+            System.out.println(readData());
+            messageArea.appendText(readData().toString());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    public String readData() {
+        try {
+            return serialPort.readString();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public void closeConnection() {
+        if (serialPort != null && serialPort.isOpened()) {
+            try {
+                serialPort.closePort();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void OnEdit(ListView.EditEvent editEvent) {
+
+    }
+
+    public void searchDevices(ActionEvent actionEvent) {
+        devices.clear();
         String[] portNames = SerialPortList.getPortNames();
-        ObservableList<String> devices = FXCollections.observableArrayList();
+
         // Print the list of available ports
         System.out.println("Available Bluetooth Devices:");
         for (String portName : portNames) {
@@ -54,5 +105,15 @@ public class DashboardController implements Initializable {
             devices.add(portName);
         }
         panelId.setItems(devices);
+    }
+
+    public void OnMouseClick(MouseEvent mouseEvent) {
+        String selectedDevice = panelId.getSelectionModel().getSelectedItem().toString();
+        if (selectedDevice != null) {
+            System.out.println("Selected Bluetooth Device: " + selectedDevice);
+            portID.setText(selectedDevice);
+        } else {
+            System.out.println("No device selected.");
+        }
     }
 }
